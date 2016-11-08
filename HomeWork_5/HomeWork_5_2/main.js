a8025ca455b2fb0f5dcc3c933a4fc5ffb1aa71f5
Taras Golotiuk <tarikmanos@gmail.com>
2016-11-08 10:40:57 +0200

    5_2 complete

--- HomeWork_5/HomeWork_5_2/main.js
+++ HomeWork_5/HomeWork_5_2/main.js
@@ -1,14 +1,14 @@
 var years;
-years=prompt('Введите ваш возраст');
+years = prompt('Введите ваш возраст');
 	console.log(typeof(years));
 if (isNaN(years)) {
 		document.write('Не верное значение, перегрузите страницу для следующей попытки');
 }
 else {
-	if (years>=0&&years<6||years>=6&&years<12||years>=12&&years<18) {
+	if (years >= 0 && years < 6 || years >= 6 && years < 12 || years >= 12 && years < 18) {
 		document.write('Вы еще не совершеннолетний, употребление алкоголя запрещено!!!');
 	}
-	else if (years<=120){
+	else if (years <= 120){
 		document.write('Вы совершеннолетний и вправе употреблять любые напитки.');
 	}
 	else {
