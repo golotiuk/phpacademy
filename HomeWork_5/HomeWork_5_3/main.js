ef6d4b8d0c4a5582de1a22da8efeed807b5ffd36
Taras Golotiuk <tarikmanos@gmail.com>
2016-11-09 11:48:36 +0200

    correction

--- HomeWork_5/HomeWork_5_3/main.js
+++ HomeWork_5/HomeWork_5_3/main.js
@@ -1,34 +1,31 @@
 var a = +prompt('Введите первое число'),
 	b = +prompt('Введите второе число'),
-	c, d;
-for (; ;) {
-	c = prompt('Ведите оператор');
+	c = prompt('Ведите оператор'),
+	d;
 	switch (c) {
 		case '+': {
-			d=a+b;
-			document.write(a+" + "+b+" = "+d);
+			d = a + b;
+			document.write(a + " + " + b + " = " + d);
 			break;
 			}
 		case '-': {
-			d=a-b;
-			document.write(a+" - "+b+" = "+d);
+			d = a - b;
+			document.write(a + " - " + b + " = " + d);
 			break;
 			}
 			case '*': {
-			d=a*b;
-			document.write(a+" * "+b+" = "+d);
+			d = a * b;
+			document.write( a + " * " + b + " = " + d);
 			break;
 			}
 			case '/': {
-			d=a/b;
-			document.write(a+" / "+b+" = "+d);
+			d = a / b;
+			document.write( a + " / " + b + " = " + d);
 			break;
 			}
 			case '%': {
-			d=a%b;
-			document.write(a+" % "+b+" = "+d);
+			d = a % b;
+			document.write( a + " % " + b + " = " + d);
 			break;
 			}
 	}
-	break;
-}
