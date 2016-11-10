05d132cd8e9431d0a714b1fd72c9981c001e53d7
Taras Golotiuk <tarikmanos@gmail.com>
2016-11-10 17:23:32 +0200

    revers finished

---
+++ HomeWork_6/HomeWork_6_2/main.js
@@ -0,0 +1,11 @@
+var a = +prompt('Введите количество элементов массива');
+var b = new Array(a);
+for (var i = 0; i < a; i++)
+    b[i] =Math.floor(Math.random()*100);
+document.write('Наш рандомный массив:<br><br>' + b.join(' | ') + '<hr width="400px" align="left">');
+for (var i = 0; i <= (b.length / 2); i++, a--) {
+    var c = b[i];
+    b[i] = b[a - 1];
+    b[a - 1] = c;
+}
+document.write('Наш зеркальный массив:<br><br>' + b.join(' | '));
