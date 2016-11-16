30fa465496ba151f0f3e986903d91fdcc07a96ff
Taras Golotiuk <tarikmanos@gmail.com>
2016-11-16 16:15:12 +0200

    cool calculator

--- HomeWork_7/HomeWork_7_2/main.js
+++ HomeWork_7/HomeWork_7_2/main.js
@@ -1,5 +1,9 @@
 var a = [];
 var person = {};
+// function error(pers) {
+//     if (pers == null)
+//     alert('не корректное значение')
+// }
 function lg() {
     var lang_arr = [];
     var language;
@@ -18,15 +22,16 @@ function ask() {
             age: +prompt("Введите свой возраст:"),
             languages: lg()
         };
+        // error(person);
         a.push(person);
         repeater = confirm("Добавить нового человека?");
     } while (repeater);
 }
 function say() {
-    var names = a.map(function(pers) {
-        return 'Имя - ' + pers.name + '<br>' + 'Фамилия - ' + pers.sname + '<br>' + 'Возраст - ' + pers.age + '<br>' + 'Владение языками - ' + pers.languages.join(', ') + '<hr>'
-    })
-    document.write(names.join(''));
+    var list = a.map(function(pers) {
+        return 'Имя - ' + pers.name + '<br>' + 'Фамилия - ' + pers.sname + '<br>' + 'Возраст - ' + pers.age + '<br>' + 'Владение языками - ' + pers.languages.join(', ')
+    });
+    document.write(list.join('<hr>'));
 }
 ask();
 say();
