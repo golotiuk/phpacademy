aa5522ee6c0becc5693922f6597aae02865812d1
Taras Golotiuk <tarikmanos@gmail.com>
2016-11-17 23:02:46 +0200

    Принимает и выводит значения, без проверок, языки НЕ массив еще

--- HomeWork_7/HomeWork_7_2/new_baza.js
+++ HomeWork_7/HomeWork_7_2/new_baza.js
@@ -1,16 +1,12 @@
 var a = [];
 var person = {};
-// var list;
 document.getElementById("add").onclick = function () {
     person = {
         name: document.getElementsByName("name")[0].value,
         sname: document.getElementsByName("sname")[0].value,
         age: document.getElementsByName("age")[0].value,
-        languages: document.getElementsByName("langs")[0].value
+        languages: document.getElementsByName("langs")[0].value.split(',')
     };
-    n = document.getElementsByName("name")[0].value;
-    console.log(n);
-    console.log(person);
     a.push(person);
     document.getElementsByName("name")[0].value = '';
     document.getElementsByName("sname")[0].value = '';
@@ -19,18 +15,12 @@ document.getElementById("add").onclick = function () {
 };
 document.getElementById("list").onclick = function () {
     var info = document.getElementById("info");
-    var lists = document.createElement("p");
+    var li = document.getElementById("lists");
+    li.innerHTML = '';
     list = a.map(function (pers) {
         return 'Имя - ' + pers.name + '<br>' + 'Фамилия - ' + pers.sname + '<br>' + 'Возраст - ' + pers.age + '<br>' + 'Владение языками - ' + pers.languages
     });
-    lists.innerHTML = list.join('<hr>');
-    console.log(typeof lists);
-    info.appendChild(lists);
+    li.innerHTML = list.join('<hr>');
+    // console.log(typeof lists);
+    info.appendChild(li);
 };
-// function list() {
-//      list = a.map(function (pers) {
-//         return 'Имя - ' + pers.name + '<br>' + 'Фамилия - ' + pers.sname + '<br>' + 'Возраст - ' + pers.age + '<br>' + 'Владение языками - ' + pers.languages
-//     });
-//     // asd = list.join('<hr>');
-// }
-console.log(a);
