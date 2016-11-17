fbfc8ce392a3156262f6557494ac387617b3af05
Taras Golotiuk <tarikmanos@gmail.com>
2016-11-17 19:32:09 +0200

    Принимает и выводит значения, без проверок, языки НЕ массив еще

--- HomeWork_7/HomeWork_7_2/new_baza.js
+++ HomeWork_7/HomeWork_7_2/new_baza.js
@@ -1,30 +1,36 @@
 var a = [];
 var person = {};
+// var list;
 document.getElementById("add").onclick = function () {
     person = {
-        name: document.getElementsByName("name"),
-        sname: document.getElementsByName("sname"),
-        age: document.getElementsByName("age"),
-        languages: document.getElementsByName("langs")
+        name: document.getElementsByName("name")[0].value,
+        sname: document.getElementsByName("sname")[0].value,
+        age: document.getElementsByName("age")[0].value,
+        languages: document.getElementsByName("langs")[0].value
     };
-
+    n = document.getElementsByName("name")[0].value;
+    console.log(n);
+    console.log(person);
     a.push(person);
+    document.getElementsByName("name")[0].value = '';
+    document.getElementsByName("sname")[0].value = '';
+    document.getElementsByName("age")[0].value = '';
+    document.getElementsByName("langs")[0].value = '';
 };
 document.getElementById("list").onclick = function () {
     var info = document.getElementById("info");
     var lists = document.createElement("p");
-    lists.innerHTML = write();
-    console.log(typeof lists);
-
-//     list = "hi dslkfdsf dsf";
-    info.appendChild(lists);
-};
-// // write();
-// // say();
-function write() {
-    var list = a.map(function (pers) {
+    list = a.map(function (pers) {
         return 'Имя - ' + pers.name + '<br>' + 'Фамилия - ' + pers.sname + '<br>' + 'Возраст - ' + pers.age + '<br>' + 'Владение языками - ' + pers.languages
     });
-    // document.write(list.join('<hr>'));
-    // alert(list)
-}
+    lists.innerHTML = list.join('<hr>');
+    console.log(typeof lists);
+    info.appendChild(lists);
+};
+// function list() {
+//      list = a.map(function (pers) {
+//         return 'Имя - ' + pers.name + '<br>' + 'Фамилия - ' + pers.sname + '<br>' + 'Возраст - ' + pers.age + '<br>' + 'Владение языками - ' + pers.languages
+//     });
+//     // asd = list.join('<hr>');
+// }
+console.log(a);
