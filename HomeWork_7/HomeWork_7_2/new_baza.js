5db63b9278f713aeacb4144a7899c4a8cc24dc92
Taras Golotiuk <tarikmanos@gmail.com>
2016-11-18 15:01:00 +0200

    Добавлено моле для пола, проверка пустое ли значение работает, но в то же время запись в массив проходит тоже

--- HomeWork_7/HomeWork_7_2/new_baza.js
+++ HomeWork_7/HomeWork_7_2/new_baza.js
@@ -4,7 +4,8 @@ document.getElementById("add").onclick = function () {
     person = {
         name: document.getElementsByName("name")[0].value,
         sname: document.getElementsByName("sname")[0].value,
-        age: document.getElementsByName("age")[0].value,
+        age: error(document.getElementsByName("age")[0].value),
+        sex: document.getElementsByName("sex")[0].value,
         languages: document.getElementsByName("langs")[0].value.split(',')
     };
     a.push(person);
@@ -18,9 +19,15 @@ document.getElementById("list").onclick = function () {
     var li = document.getElementById("lists");
     li.innerHTML = '';
     list = a.map(function (pers) {
-        return 'Имя - ' + pers.name + '<br>' + 'Фамилия - ' + pers.sname + '<br>' + 'Возраст - ' + pers.age + '<br>' + 'Владение языками - ' + pers.languages
+        return 'Имя - ' + pers.name + '<br>' + 'Фамилия - ' + pers.sname + '<br>' + 'Пол - ' + pers.sex + '<br>' + 'Возраст - ' + pers.age + '<br>' + 'Владение языками - ' + pers.languages
     });
     li.innerHTML = list.join('<hr>');
-    // console.log(typeof lists);
     info.appendChild(li);
+};
+function error(arg) {
+    if (arg == "") {
+       // return console.log('ERROR');
+       // console.log(arg);
+       return alert('не корректное значение');
+    }
 };
