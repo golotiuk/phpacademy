6ca6cc5f49d68d14d42d0e158db36d64f702c5ca
Taras Golotiuk <tarikmanos@gmail.com>
2016-11-21 10:50:12 +0200

    работает проверка на заполнение полей и на число в возрасте, радиобатоны пока не проверяет

--- HomeWork_7/HomeWork_7_2/new_baza.js
+++ HomeWork_7/HomeWork_7_2/new_baza.js
@@ -3,7 +3,47 @@ var person = {};
 var x;
 document.getElementById("add").onclick = function () {
     valid(document.getElementsByClassName("validate"));
-
+};
+document.getElementById("list").onclick = function () {
+    var info = document.getElementById("info");
+    var li = document.getElementById("lists");
+    li.innerHTML = '';
+    list = a.map(function (pers) {
+        return 'Имя - ' + pers.name + '<br>' + 'Фамилия - ' + pers.sname + '<br>' + 'Пол - ' + pers.sex + '<br>' + 'Возраст - ' + pers.age + '<br>' + 'Владение языками - ' + pers.languages
+    });
+    li.innerHTML = list.join('<hr>');
+    info.appendChild(li);
+};
+function check(arg) {
+    var res = arg;
+    for (var i = 0; i < res.length; i++) {
+        if (res[i].checked == true) {
+            return res[i].value;
+        }
+    }
+}
+function valid(arg) {
+    var x;
+    for (i = 0; i < arg.length; i++) {
+        console.log(typeof arg[i].value);
+        if (arg[i].value == "") {
+            x = false;
+            alert("Не верные значение!");
+            break;
+        }
+    }
+    if ( isNaN(document.getElementsByName("age")[0].value)){
+        alert("not number");
+        x = false;
+        return x;
+    }
+    else {
+        alert('Добавлено');
+    }
+    if (x == false) return false;
+    else write();
+}
+function write() {
     person = {
         name: document.getElementsByName("name")[0].value,
         sname: document.getElementsByName("sname")[0].value,
@@ -16,52 +56,4 @@ document.getElementById("add").onclick = function () {
     document.getElementsByName("sname")[0].value = '';
     document.getElementsByName("age")[0].value = '';
     document.getElementsByName("langs")[0].value = '';
-};
-document.getElementById("list").onclick = function () {
-    var info = document.getElementById("info");
-    var li = document.getElementById("lists");
-    li.innerHTML = '';
-    list = a.map(function (pers) {
-        return 'Имя - ' + pers.name + '<br>' + 'Фамилия - ' + pers.sname + '<br>' + 'Пол - ' + pers.sex + '<br>' + 'Возраст - ' + pers.age + '<br>' + 'Владение языками - ' + pers.languages
-    });
-    li.innerHTML = list.join('<hr>');
-    info.appendChild(li);
-};
-
-function check(arg) {
-    var res = arg;
-    for (var i = 0; i < res.length; i++) {
-        if (res[i].checked == true) {
-            return res[i].value;
-        }
-    }
-}
-// function error(arg) {
-//     if (arg == "") {
-//         x = false;
-//         return alert(x);
-//     }
-//     else
-//         return person.name = arg;
-// }
-// function err(arg) {
-//     console.log(arg)
-//     for (field in arg) {
-//         if (arg[field] == "") {
-//             arg = 0;
-//             return alert("!");
-//         }
-//     }
-// }
-function valid(arg) {
-    for (i = 0; i < arg.length; i++) {
-        if (arg[i].value == "") {
-
-            return alert("not valid")
-        }
-        else {
-            alert("Готово");
-            continue;
-        }
-    }
 }
