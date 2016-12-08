2a44ac015054905e989a05e9a80a3fd62f44c3e6
Taras Golotiuk <tarikmanos@gmail.com>
2016-12-08 11:16:20 +0200

    fix

--- HomeWork_7/HomeWork_7_1/main_with_cb.js
+++ HomeWork_7/HomeWork_7_1/main_with_cb.js
@@ -3,19 +3,19 @@ var a = +prompt('Введите первое число'),
     c = +prompt('Введите второе число');
 function add(a, c) {
     return a + c;
-};
+}
 function sub(a, c) {
     return a - c;
-};
+}
 function div(a, c) {
     return a / c;
-};
+}
 function mult(a, c) {
     return a * c;
-};
+}
 function ost(a, c) {
     return a % c;
-};
+}
 function show(cb, a, c) {
     var result = cb(a, c);
     document.write("Ваш результат: " + a + " " + b + " " + c + " = " + result)
